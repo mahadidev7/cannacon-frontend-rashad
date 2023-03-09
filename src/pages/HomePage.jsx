e84d35f45a03fc14e0444259c6242853ce2191a3
mahadidev7 <mahadidev7@gmail.com>
2023-03-09 15:35:21 +0600

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="h-screen w-screen bg-slate-200">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
